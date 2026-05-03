1678d103c89fcd540b90bff06c54525064ed1868
Emmy Etah <N1254885@my.ntu.ac.uk>
2026-05-04 00:47:22 +0100

    adding final changes to git

--- firmware/ECLIPSE_ESP32/ECLIPSE_ESP32.ino
+++ firmware/ECLIPSE_ESP32/ECLIPSE_ESP32.ino
@@ -3,71 +3,98 @@
 
 SensorManager sensorManager;
 
-void setup() {
-    Serial.begin(115200);
-    delay(2000); // give serial time to connect
+// ---------------- Geiger settings ----------------
+const int GEIGER_PIN = 27;
 
-    if (!sensorManager.begin()) {
+// Telemetry output every 2 seconds
+const unsigned long TELEMETRY_INTERVAL_MS = 2000;
+
+// Radiation updates every 30 seconds
+const unsigned long RADIATION_INTERVAL_MS = 30000;
+
+volatile unsigned long geigerPulses = 0;
+
+unsigned long lastTelemetryTime = 0;
+unsigned long lastRadiationUpdate = 0;
+
+unsigned long radiationCPM = 0;        // value printed to ECLIPSE
+unsigned long previousRadiationCPM = 0;
+unsigned long lastRadiationWindowPulses = 0;
+
+void IRAM_ATTR onGeigerPulse() {
+    geigerPulses++;
+}
+
+void updateRadiationCPM() {
+    noInterrupts();
+    unsigned long pulses = geigerPulses;
+    geigerPulses = 0;
+    interrupts();
+
+    lastRadiationWindowPulses = pulses;
+
+    unsigned long currentCPM = (pulses * 60000UL) / RADIATION_INTERVAL_MS;
+
+    // Only update displayed radiation value if it changed
+    if (currentCPM != previousRadiationCPM) {
+        radiationCPM = currentCPM;
+        previousRadiationCPM = currentCPM;
     }
 }
 
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+
+    sensorManager.begin();
+
+    pinMode(GEIGER_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(GEIGER_PIN), onGeigerPulse, RISING);
+
+    lastTelemetryTime = millis();
+    lastRadiationUpdate = millis();
+}
+
 void loop() {
-    SensorSnapshot snap = sensorManager.readAll();
+    unsigned long now = millis();
 
-    // timestamp
-    Serial.print(snap.timestampMs);
-    Serial.print(",");
-
-    // BME680 temperature
-    if (snap.bme680.valid) {
-        Serial.print(snap.bme680.temperatureC, 2);
-    } else {
-        Serial.print("NA");
+    if (now - lastRadiationUpdate >= RADIATION_INTERVAL_MS) {
+        updateRadiationCPM();
+        lastRadiationUpdate = now;
     }
-    Serial.print(",");
 
-    // BME680 humidity
-    if (snap.bme680.valid) {
-        Serial.print(snap.bme680.humidityRH, 2);
-    } else {
-        Serial.print("NA");
+    if (now - lastTelemetryTime >= TELEMETRY_INTERVAL_MS) {
+        lastTelemetryTime = now;
+
+        SensorSnapshot snap = sensorManager.readAll();
+
+        Serial.print(snap.timestampMs);
+        Serial.print(",");
+
+        if (snap.bme680.valid) Serial.print(snap.bme680.temperatureC, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        if (snap.bme680.valid) Serial.print(snap.bme680.humidityRH, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        if (snap.bme680.valid) Serial.print(snap.bme680.pressurehPa, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        if (snap.scd30.valid) Serial.print(snap.scd30.co2ppm, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        if (snap.scd30.valid) Serial.print(snap.scd30.temperatureC, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        if (snap.scd30.valid) Serial.print(snap.scd30.humidityRH, 2);
+        else Serial.print("NA");
+        Serial.print(",");
+
+        Serial.println(radiationCPM);
     }
-    Serial.print(",");
-
-    // BME680 pressure
-    if (snap.bme680.valid) {
-        Serial.print(snap.bme680.pressurehPa, 2);
-    } else {
-        Serial.print("NA");
-    }
-    Serial.print(",");
-
-    // SCD30 CO2
-    if (snap.scd30.valid) {
-        Serial.print(snap.scd30.co2ppm, 2);
-    } else {
-        Serial.print("NA");
-    }
-    Serial.print(",");
-
-    // SCD30 temperature
-    if (snap.scd30.valid) {
-        Serial.print(snap.scd30.temperatureC, 2);
-    } else {
-        Serial.print("NA");
-    }
-    Serial.print(",");
-
-    // SCD30 humidity
-    if (snap.scd30.valid) {
-        Serial.print(snap.scd30.humidityRH, 2);
-    } else {
-        Serial.print("NA");
-    }
-    Serial.print(",");
-
-    // Radiation placeholder for now
-    Serial.println(0);
-
-    delay(2000); // read every 2 seconds
 }
